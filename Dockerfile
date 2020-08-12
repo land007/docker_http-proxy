371bdfcb7e985ec05d8d1cd0d805298191cc52bf
阿七 <yiqiujia@hotmail.com>
2020-08-12 09:00:55 +0800

    test

--- Dockerfile
+++ Dockerfile
@@ -4,23 +4,58 @@ MAINTAINER Jia Yiqiu <yiqiujia@hotmail.com>
 
 RUN echo $(date "+%Y-%m-%d_%H:%M:%S") >> /.image_times && \
 	echo $(date "+%Y-%m-%d_%H:%M:%S") > /.image_time && \
-	echo "land007/node-http-proxy" >> /.image_names && \
-	echo "land007/node-http-proxy" > /.image_name
+	echo "land007/http-proxy" >> /.image_names && \
+	echo "land007/http-proxy" > /.image_name
 
-ADD proxy.js /node_/server.js
 RUN . $HOME/.nvm/nvm.sh && cd / && npm install basic-auth
+ADD proxy.js /node_/server.js
+ADD node/web-outgoing.js /node_modules/http-proxy/lib/http-proxy/passes/web-outgoing.js
 
 ENV username=land007 \
 	password=fcea920f7412b5da7be0cf42b8c93759 \
+	http_proxy_domains="192.168.1.1:1080 192.168.1.1:1080" \
 	http_proxy_paths="/api/ /" \
 	http_proxy_hosts="192.168.1.218 192.168.1.218" \
 	http_proxy_ports="8080 3000" \
+	http_proxy_pretends="true true" \
+	ws_proxy_domains="192.168.1.1:1080" \
 	ws_proxy_paths="/api/" \
 	ws_proxy_hosts="192.168.1.218" \
 	ws_proxy_ports="8080"
 
 ADD node/start.sh /node_/
+ADD node/cert /node_/cert
+RUN sed -i 's/\r//' /node_/start.sh
+
+ENV DOMAIN_NAME=voice.qhkly.com
+EXPOSE 80
+EXPOSE 443
+
 CMD /check.sh /node && /node/start.sh
 
-#docker build -t land007/node-http-proxy:latest .
-#docker rm -f node-http-proxy ; sudo rm -rf ~/docker/node-http-proxy ; docker run -it --privileged --restart=always -v ~/docker/node-http-proxy:/node -p 10080:80 --name node-http-proxy land007/node-http-proxy:latest
+# 代理，有问题啊/node_modules/http-proxy/lib/http-proxy/passes/web-outgoing.js:54:24
+#//var target = url.parse(options.target);
+#var target = options.target;
+
+#docker build -t land007/http-proxy:latest .
+#docker save -o http-proxy.tar land007/http-proxy:latest
+
+#docker rm -f http-proxy ; sudo rm -rf ~/docker/http-proxy ; docker run -it --privileged --restart=always -v ~/docker/http-proxy:/node -p 10080:80 --name http-proxy land007/http-proxy:latest
+#docker rm -f http-proxy ; sudo rm -rf ~/docker/http-proxy ; docker run -it --privileged --restart=always -v ~/docker/http-proxy:/node -e "http_proxy_paths=/" -e "http_proxy_hosts=192.168.181.134" -e "http_proxy_ports=7070" -p 10080:80 -p 10443:443 --name http-proxy land007/http-proxy:latest
+#docker rm -f http-proxy ; sudo rm -rf ~/docker/http-proxy ; docker run -it --privileged --rm -e "http_proxy_paths=/" -e "http_proxy_hosts=192.168.181.134" -e "http_proxy_ports=7070" -e "password=" -p 10080:80 -p 10443:443 --name http-proxy land007/http-proxy:latest
+#docker rm -f http-proxy ; sudo rm -rf ~/docker/http-proxy ; docker run -it --privileged --rm -e "DOMAIN_NAME=www.gjxt.xyz" -e "http_proxy_paths=/" -e "http_proxy_hosts=192.168.181.134" -e "http_proxy_ports=7070" -e "password=" -p 10080:80 -p 10443:443 --name http-proxy land007/http-proxy:latest
+#docker run -it --privileged --rm -e "DOMAIN_NAME=www.gjxt.xyz" -e "http_proxy_paths=/" -e "http_proxy_hosts=192.168.181.134" -e "http_proxy_ports=8080" -e "password=" -p 10080:80 -p 6060:443 --name http-proxy land007/http-proxy:latest
+
+#docker run -it --privileged --restart=always -e "DOMAIN_NAME=www.gjxt.xyz" -e "http_proxy_paths=/" -e "http_proxy_hosts=192.168.181.134" -e "http_proxy_ports=6061" -e "password=" -p 10080:80 -p 6060:443 --name http-proxy land007/http-proxy:latest
+#docker run -it --privileged --restart=always -e "DOMAIN_NAME=www.gjxt.xyz" -e "http_proxy_paths=/scm/" -e "http_proxy_hosts=192.168.181.134" -e "http_proxy_ports=8081" -e "password=" -p 10081:80 -p 8080:443 --name http-proxy1 land007/http-proxy:latest
+
+#docker run -it --privileged --restart=always -e "DOMAIN_NAME=www.gjxt.xyz" -e "http_proxy_paths=/" -e "http_proxy_hosts=192.168.1.228" -e "http_proxy_ports=3001" -e "password=" -p 10081:80 -p 6060:443 --name http-proxy1 land007/http-proxy:latest
+#docker run -it --privileged --rm -e "DOMAIN_NAME=www.gjxt.xyz" -e "http_proxy_paths=/" -e "http_proxy_hosts=172.19.80.1" -e "http_proxy_ports=3001" -e "ws_proxy_paths=/" -e "ws_proxy_hosts=172.19.80.1" -e "ws_proxy_ports=3001" -e "password=" -p 10081:80 -p 6060:443 --name http-proxy1 land007/http-proxy:latest
+
+#systemctl start firewalld.service
+#docker rm -f http-proxy; docker run -it --privileged --restart=always -e "DOMAIN_NAME=www.gjxt.xyz" -e "http_proxy_paths=/" -e "http_proxy_hosts=192.168.181.134" -e "http_proxy_ports=6061" -e "ws_proxy_paths=/" -e "ws_proxy_hosts=192.168.181.134" -e "ws_proxy_ports=6061" -e "username=gjxt" -p 10080:80 -p 6060:443 --name http-proxy land007/http-proxy:latest
+#systemctl stop firewalld.service
+
+#[root@bogon Desktop]# systemctl stop firewalld.service
+#[root@bogon Desktop]# systemctl start firewalld.service
+
--- proxy.js
+++ proxy.js
@@ -1,18 +1,32 @@
 const httpProxy = require('http-proxy');
 const http = require('http');
+const https = require('https');
 const basicAuth = require('basic-auth');
 const crypto = require('crypto');
 const url = require("url");
+const fs = require("fs");
+const path = require("path");
+const sep = path.sep;
 
 var username = process.env['username'] || 'land007';
-var password = process.env['password'] || 'fcea920f7412b5da7be0cf42b8c93759';
+var password = process.env['password'] || '';
+
+var http_proxy_domains = (process.env['http_proxy_domains'] || '').split(' ');
 var http_proxy_paths = (process.env['http_proxy_paths'] || '').split(' ');
 var http_proxy_hosts = (process.env['http_proxy_hosts'] || '').split(' ');
 var http_proxy_ports = (process.env['http_proxy_ports'] || '').split(' ');
-var ws_proxy_hosts = (process.env['ws_proxy_hosts'] || '').split(' ');
+var http_proxy_pretends = (process.env['http_proxy_pretends'] || '').split(' ');
+
+var ws_proxy_domains = (process.env['ws_proxy_domains'] || '').split(' ');
 var ws_proxy_paths = (process.env['ws_proxy_paths'] || '').split(' ');
 var ws_proxy_hosts = (process.env['ws_proxy_hosts'] || '').split(' ');
 var ws_proxy_ports = (process.env['ws_proxy_ports'] || '').split(' ');
+
+var domainName = process.env['DOMAIN_NAME'] || "voice.qhkly.com"; // e.g., "westus"
+const options = {
+	key: fs.readFileSync(__dirname + sep + 'cert' + sep + domainName + '_key.key'),
+	cert: fs.readFileSync(__dirname + sep + 'cert' + sep + domainName + '_chain.crt')
+};
 
 var send401 = function(res) {
 	res.statusCode = 401;
@@ -20,39 +34,55 @@ var send401 = function(res) {
 	res.end('<html><body>Need some creds son</body></html>');
 };
 
-var proxyServer = http.createServer(function (req, res) {
+var requestListener = function (req, res) {
+	var host = req.headers.host;
 	let pathname = url.parse(req.url).pathname;
-	let user = basicAuth(req);
-	if (!user) {
-		send401(res);
-		return;
-	}
-	let md5 = crypto.createHash('md5');
-	if (user.pass === undefined) {
-		md5.update('undefined');
-	} else {
-		md5.update(user.pass);
-	}
-	let pass = md5.digest('hex');
-	if (user.name !== username || pass !== password) {
-		send401(res);
-		return;
+	if(password != '') {
+		let user = basicAuth(req);
+		if (!user) {
+			send401(res);
+			return;
+		}
+		let md5 = crypto.createHash('md5');
+		if (user.pass === undefined) {
+			md5.update('undefined');
+		} else {
+			md5.update(user.pass);
+		}
+		let pass = md5.digest('hex');
+		if (user.name !== username || pass !== password) {
+			send401(res);
+			return;
+		}
 	}
 	let have_http_proxy_path = false;
 	for(let h in http_proxy_paths) {
-		if(pathname.indexOf(http_proxy_paths[h]) == 0) {
+		if(pathname.indexOf(http_proxy_paths[h]) == 0 && (http_proxy_domains[h] == '' || http_proxy_domains[h] == host)) {
 			have_http_proxy_path = true;
-			let proxy = httpProxy.createProxyServer({
-				target: {
-					host: http_proxy_hosts[h],
-					port: http_proxy_ports[h]
-				},
-				ws: false
-			});
-			proxy.on('proxyReq', function(proxyReq, req, res, options) {
-				proxyReq.setHeader('Host', http_proxy_hosts[h] + ':' + http_proxy_ports[h]);
-			});
-			proxy.web(req, res);
+			if(http_proxy_pretends[h] == 'true') {
+				let proxy = httpProxy.createProxyServer({
+				    hostRewrite: http_proxy_hosts[h],
+				    autoRewrite: true,
+					target: {
+						host: http_proxy_hosts[h],
+						port: http_proxy_ports[h]
+					},
+					ws: false
+				});
+				proxy.on('proxyReq', function(proxyReq, req, res, options) {
+					proxyReq.setHeader('Host', http_proxy_hosts[h] + ':' + http_proxy_ports[h]);
+				});
+				proxy.web(req, res);
+			} else {
+				let proxy = httpProxy.createProxyServer({
+					target: {
+						host: http_proxy_hosts[h],
+						port: http_proxy_ports[h]
+					},
+					ws: false
+				});
+				proxy.web(req, res);
+			}
 			break;
 		}
 	}
@@ -62,12 +92,13 @@ var proxyServer = http.createServer(function (req, res) {
 		});
 		res.end('Welcome to my server!');
 	}
-});
+};
 
-proxyServer.on('upgrade', function (req, socket, head) {
+var upgrade = function (req, socket, head) {
+	var host = req.headers.host;
 	let pathname = url.parse(req.url).pathname;
 	for(let w in ws_proxy_paths) {
-		if(pathname.indexOf('/api/') == 0) {
+		if(pathname.indexOf(ws_proxy_paths[w]) == 0 && (ws_proxy_domains[w] == '' || ws_proxy_domains[w] == host)) {
 			let proxy = new httpProxy.createProxyServer({
 				target : {
 					host :  ws_proxy_hosts[w],
@@ -79,7 +110,16 @@ proxyServer.on('upgrade', function (req, socket, head) {
 			break;
 		}
 	}
-});
+};
+
+var proxysServer = https.createServer(options, requestListener);
+var proxyServer = http.createServer(requestListener);
+
+proxysServer.on('upgrade', upgrade);
+proxyServer.on('upgrade', upgrade);
+
+proxysServer.listen(443);
+console.log("listen 443");
 
 proxyServer.listen(80);
 console.log("listen 80");
