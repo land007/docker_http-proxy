fdb04463a14182e9a82b3d389d4052e03cc17076
阿七 <yiqiujia@hotmail.com>
2022-12-27 17:07:38 +0800

    test

--- node/proxy.js
+++ node/proxy.js
@@ -127,6 +127,92 @@ const init = async function() {
 init();
 setInterval(init, 5000);
 
+const check = function(req, h, _token) {
+	// 检查session
+	let login_name;
+	if (max_session > 0 && _token) {
+		login_name = req.session.get('login_name');
+		consoleLog('login_name', login_name);
+	}
+	// 没有登录
+	if (login_name === undefined) {
+		let users;
+		if (users_list !== undefined) {
+			// 得到用户名密码对应表
+			users = users_list[h];
+		}
+		// 可以使用用户名密码参数
+		if (users === undefined) {
+			// 传参也支持多用户名密码形式
+			let _usernames = (usernames[h] ? usernames[h] : username).split('|');
+			let _passwords = (passwords[h] ? passwords[h] : password).split('|');
+			consoleLog('_usernames', _usernames);
+			consoleLog('_passwords', _passwords);
+			// 支持没有密码
+			if (!(_passwords.length == 1 && _passwords[0] == '')) {
+				users = {};
+				for (let _p in _passwords) {
+					let _username = _usernames[_p];
+					let _password = _passwords[_p];
+					// 统一成用户名密码对象表
+					users[_username] = _password;
+				}
+			}
+		}
+		// 如果需要验证
+		if (users !== undefined && users !== null) {
+			consoleLog('users', users);
+			// 获取请求中的用户名密码
+			let user = basicAuth(req);
+			consoleLog('user', user);
+			// 没有的要求给
+			if (!user) {
+				return false;
+			}
+			// 验证的用户名必须有
+			consoleLog('users[user.name]', users[user.name]);
+			if (users[user.name] === undefined) {
+				return false;
+			}
+			// 验证的密码是md5，防止泄漏
+			let md5 = crypto.createHash('md5');
+			if (user.pass === undefined) {
+				md5.update('undefined');
+			} else {
+				md5.update(user.pass);
+			}
+			let pass = md5.digest('hex');
+			consoleLog('pass', pass);
+			// 密码需正确
+			if (pass !== users[user.name]) {
+				return false;
+			}
+			// 多登录支持
+			if (max_session > 0 && _token) {
+				// 把session保存在内存中
+				req.session.put('login_name', user.name);
+				if (_userSession[user.name] === undefined) {
+					_userSession[user.name] = [];
+				}
+				_userSession[user.name].unshift(_token);
+				// 超出数量的session删除
+				if (_userSession[user.name].length > max_session) {
+					_userSession[user.name].pop();
+				}
+			}
+		}
+	} else {// 登录过
+		let tokens = _userSession[login_name];
+		// 检查内存中的session是否允许
+		consoleLog('tokens', tokens);
+		if (tokens === undefined || !tokens.includes(_token)) {
+			req.session.forget('login_name');
+			return false;
+		}
+	}
+	return true;
+}
+
 const _requestListener = async function(req, res) {
 	//let ip = getClientIp(req);
 	let host = req.headers.host;
@@ -139,76 +225,14 @@ const _requestListener = async function(req, res) {
 	}
 	let have_http_proxy_path = false;
 	for (let h in http_proxy_paths) {
+		// 路径及域名验证
 		if (pathname.indexOf(http_proxy_paths[h]) == 0 && (http_proxy_domains[h] == '' || http_proxy_domains[h] == host)) {
-			let login_name;
-			if (max_session > 0) {
-				login_name = req.session.get('login_name');
-				consoleLog('login_name', login_name);
+			// 检查登录信息
+			if(!check(req, h, _token)) {
+				send401(res);
+				return;
 			}
-			if (login_name === undefined) {// 没有登录
-				let users;
-				if (users_list !== undefined) {
-					users = users_list[h];
-				}
-				if (users === undefined) {
-					let _usernames = (usernames[h] ? usernames[h] : username).split('|');
-					let _passwords = (passwords[h] ? passwords[h] : password).split('|');
-					consoleLog('_usernames', _usernames);
-					consoleLog('_passwords', _passwords);
-					if (!(_passwords.length == 1 && _passwords[0] == '')) {
-						users = {};
-						for (let _p in _passwords) {
-							let _username = _usernames[_p];
-							let _password = _passwords[_p];
-							users[_username] = _password;
-						}
-					}
-				}
-				if (users !== undefined && users !== null) {
-					consoleLog('users', users);
-					let user = basicAuth(req);
-					consoleLog('user', user);
-					if (!user) {
-						send401(res);
-						return;
-					}
-					consoleLog('users[user.name]', users[user.name]);
-					if (users[user.name] === undefined) {
-						send401(res);
-						return;
-					}
-					let md5 = crypto.createHash('md5');
-					if (user.pass === undefined) {
-						md5.update('undefined');
-					} else {
-						md5.update(user.pass);
-					}
-					let pass = md5.digest('hex');
-					consoleLog('pass', pass);
-					if (pass !== users[user.name]) {
-						send401(res);
-						return;
-					}
-					if (max_session > 0) {
-						req.session.put('login_name', user.name);
-						if (_userSession[user.name] === undefined) {
-							_userSession[user.name] = [];
-						}
-						_userSession[user.name].unshift(_token);
-						if (_userSession[user.name].length > max_session) {
-							_userSession[user.name].pop();
-						}
-					}
-				}
-			} else {// 登录过
-				let tokens = _userSession[login_name];
-				consoleLog('tokens', tokens);
-				if (tokens === undefined || !tokens.includes(_token)) {
-					req.session.forget('login_name');
-					send401(res);
-					return;
-				}
-			}
+			// 都检查通过了可以代理
 			have_http_proxy_path = true;
 			if (http_proxy_pretends[h] && http_proxy_pretends[h] == 'true') {
 				let proxy = httpProxy.createProxyServer({
@@ -241,6 +265,7 @@ const _requestListener = async function(req, res) {
 			break;
 		}
 	}
+	// 没有命中
 	if (!have_http_proxy_path) {
 		res.writeHead(200, {
 			'Content-Type': 'text/plain'
@@ -280,9 +305,30 @@ const netListener = function(socket) {
 };
 
 const upgrade = function(req, socket, head) {
+	if (max_session > 0) {
+		nodeSession.startSession(req, {end: function(){}}, function() {
+			_upgrade(req, socket, head);
+		});
+	} else {
+		_upgrade(req, socket, head);
+	}
+};
+
+const _upgrade = function(req, socket, head) {
 	let host = req.headers.host;
 	let pathname = url.parse(req.url).pathname;
+	let _token;
+	if (max_session > 0) {
+		let _session = req.session.all();
+		_token = _session._token;
+		consoleLog('_token', _token);
+	}
 	for (let w in ws_proxy_paths) {
+		// 检查登录信息
+		if(!check(req, w, _token)) {
+			//send401(res);
+			return;
+		}
 		if (pathname.indexOf(ws_proxy_paths[w]) == 0 && (ws_proxy_domains[w] == '' || ws_proxy_domains[w] == host)) {
 			let proxy = new httpProxy.createProxyServer({
 				target: {
